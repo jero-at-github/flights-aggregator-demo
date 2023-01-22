79b01b815a18fac7aa773c79459d41e694ceabce
Jero <jlopeznavarro@gmail.com>
2023-01-22 13:49:00 +0100

    Added controller test. Modified e2e test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Flight } from './flights/flights.interface';
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/common';
 
 describe('AppController', () => {
   let appController: AppController;
 
-  beforeEach(async () => {
+  beforeEach(async () => {  
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, CacheModule.register(),],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +19,36 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return the flights', () => {
-      // expect(appController.getHello()).toBe('Hello World!');
+    it('should return the flights or an expected error', async () => {
+      
+      appController.cacheEnabled = false;
+      let response: Flight[];
+
+      // since we can get a combination of succes and error requests,
+      // we try couple of times to get the chance to test the different cases
+      for (let i: number = 0; i < 10; i ++) {        
+        let timerStart: Date;
+        let timerEnd: Date;
+
+        try {
+          timerStart = new Date();
+          response = await appController.getFlights();
+          timerEnd = new Date();
+          
+          expect(response.length).toBeGreaterThanOrEqual(5);          
+          expect(Math.abs(timerEnd.getTime() - timerStart.getTime()) < 1000).toBeTruthy();
+        } 
+        catch(error) {
+          // if the request response is later than 1 second we exepect an error
+          timerEnd = new Date();
+          expect(Math.abs(timerEnd.getTime() - timerStart.getTime()) >= 1000).toBeTruthy();
+          expect(error['status']).toBe(500);
+          expect(
+            error['message'] === 'No flight sources available at the moment' ||
+            error['message'] === 'Time limit exceeded'
+          ).toBeTruthy();          
+        }                        
+      }
     });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { Flight } from 'src/flights/flights.interface';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -16,24 +15,22 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', async () => {
-    // since we can get a combination of succes and error requests,
-    // we try couple of times to get the chance to test both cases
-    for (let i: number = 0; i < 5; i ++) {
-      let timerStart: Date = new Date();
-      let response = await request(app.getHttpServer()).get('/flights');
-      let timerEnd: Date = new Date();
+  it('/ (GET)', async () => {    
+    let timerStart: Date = new Date();
+    let response = await request(app.getHttpServer()).get('/flights');
+    let timerEnd: Date = new Date();
 
-      // if the request response is later than 1 second we exepect an error
-      if (Math.abs(timerEnd.getTime() - timerStart.getTime()) >= 1000) {        
-        expect(response.status == 500);
-        expect(response.body['statusCode']).toBe(500);
-        expect(response.body['message']).toBe('Time limit exceeded');
-      } else {
-        // if succeded, we expect at least the data of one of the requests (5)
-        expect(response.status == 200);
-        expect(response.body.length).toBeGreaterThanOrEqual(5);
-      }      
-    }    
+    // if the request response is later than 1 second we exepect an error
+    if (Math.abs(timerEnd.getTime() - timerStart.getTime()) >= 1000) {        
+      expect(response.status == 500);        
+      expect(
+        response.body['message'] === 'No flight sources available at the moment' ||
+        response.body['message'] === 'Time limit exceeded'
+      ).toBeTruthy();
+    } else {
+      // if succeded, we expect at least the data of one of the requests (5)
+      expect(response.status == 200);
+      expect(response.body.length).toBeGreaterThanOrEqual(5);
+    }              
   });
 });
