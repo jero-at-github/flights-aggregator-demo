53bb6de5e59e38c3a321991ca8629214c91c0445
Jero <jlopeznavarro@gmail.com>
2023-01-22 11:27:07 +0100

    Added e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Flight } from 'src/flights/flights.interface';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,24 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    // since we can get a combination of succes and error requests,
+    // we try couple of times to get the chance to test both cases
+    for (let i: number = 0; i < 5; i ++) {
+      let timerStart: Date = new Date();
+      let response = await request(app.getHttpServer()).get('/flights');
+      let timerEnd: Date = new Date();
+
+      // if the request response is later than 1 second we exepect an error
+      if (Math.abs(timerEnd.getTime() - timerStart.getTime()) >= 1000) {        
+        expect(response.status == 500);
+        expect(response.body['statusCode']).toBe(500);
+        expect(response.body['message']).toBe('Time limit exceeded');
+      } else {
+        // if succeded, we expect at least the data of one of the requests (5)
+        expect(response.status == 200);
+        expect(response.body.length).toBeGreaterThanOrEqual(5);
+      }      
+    }    
   });
 });
